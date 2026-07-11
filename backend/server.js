48c7d4e27b9d17504699bd3ca7b034c90589b238
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-07-11 23:47:23 +0530

    Increase express payload body size limit to 10mb for profile photo uploads

--- backend/server.js
+++ backend/server.js
@@ -39,8 +39,8 @@ app.use(
 );
 
 // Body Parser Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Mount API Routes
 app.use('/api', apiRoutes);
